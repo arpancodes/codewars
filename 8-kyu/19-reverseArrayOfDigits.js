0c34dcce233a609154483efc25b60da1cbabe799
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-02-15 21:59:35 +0530

    did rever array of digits problem

---
+++ 8-kyu/19-reverseArrayOfDigits.js
@@ -0,0 +1,19 @@
+/*
+Convert number to reversed array of digits
+Given a random number:
+
+C#: long;
+C++: unsigned long;
+You have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+*/
+
+const digitize = n =>
+	('' + n)
+		.split('')
+		.map(Number)
+		.reverse();
+
+console.log(digitize(348597));
